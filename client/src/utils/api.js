31290ea02e187de19ef7039a72c64bc6c1b64b65
Sitouxz <yehezkielowen0901@gmail.com>
2024-04-03 01:31:16 +0800

    Update API base URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '34.126.90.234:3000/'
+  baseURL: 'https://uteach.up.railway.app/'
   // baseURL: 'http://localhost:5000/'
 });
 
